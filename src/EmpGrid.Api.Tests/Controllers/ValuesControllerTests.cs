2df2d98839a14cdb018b718b2ea2a6bc3b4d72ff
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-05-21 20:39:48 +0200

    Scaffolded test project for API

---
+++ src/EmpGrid.Api.Tests/Controllers/ValuesControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace EmpGrid.Api.Controllers
+{
+    public class ValuesControllerTests
+    {
+        [Fact]
+        public void Get_returns_hard_coded_string()
+        {
+            var sut = new ValuesController();
+            var result = sut.Get(123);
+            result.Should().Be("value");
+        }
+    }
+}
