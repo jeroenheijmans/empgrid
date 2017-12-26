5779a9c7f705fa96139945bede56fe5baebf4aa5
Jeroen Heijmans <github@jeroenheijmans.nl>
2017-12-26 21:53:37 +0100

    Start work on edit-features

--- src/EmpGrid.Api/wwwroot/app/MonolythicPrototype.js
+++ src/EmpGrid.Api/wwwroot/app/MonolythicPrototype.js
@@ -19,20 +19,33 @@
     }
     class EmpVm {
         constructor(data, mediums) {
-            this.name = data.name;
-            this.emailAddress = data.emailAddress;
-            this.mailto = `mailto:${this.emailAddress}`;
-            this.showMailtoLink = !!this.emailAddress;
-            this.tagLine = data.tagLine || "No tag line provided...";
-            this.presences = data.presences.map(p => new PresenceVm(p, mediums));
+            this.name = ko.observable("");
+            this.emailAddress = ko.observable("");
+            this.tagLine = ko.observable("");
+            this.presences = ko.observableArray([]);
+
+            this.mailto = ko.pureComputed(() => `mailto:${this.emailAddress()}`);
+            this.showMailtoLink = ko.pureComputed(() => !!this.emailAddress());
+            this.tagLineDisplay = ko.pureComputed(() => this.tagLine() || "No tag line provided...");
+
+            this._resetDto = data;
+            this._mediums = mediums;
+            this.reset();
+        }
+
+        reset() {
+            this.name(this._resetDto.name);
+            this.emailAddress(this._resetDto.emailAddress);
+            this.tagLine(this._resetDto.tagLine);
+            this.presences(this._resetDto.presences.map(p => new PresenceVm(p, this._mediums)));
         }
     }
 
     class GridVm {
         constructor(data) {
-            this.emps = data.emps.map(e => new EmpVm(e, data.mediums));
+            this.emps = ko.observableArray(data.emps.map(e => new EmpVm(e, data.mediums)));
 
-            var nrOfCols = Math.min(8, Math.round(Math.sqrt(this.emps.length)));
+            var nrOfCols = Math.min(8, Math.round(Math.sqrt(this.emps().length)));
             this.colCss = `repeat(${nrOfCols}, 1fr)`;
         }
     }
@@ -40,13 +53,41 @@
     class RootVm {
         constructor() {
             this.grid = ko.observable();
+
+            this.isInEditMode = ko.observable(false);
+            this.empInEditMode = ko.observable(null);
+
             this.reLoadGrid();
         }
 
         reLoadGrid() {
             GET("/grid").then(data => {
                 this.grid(new GridVm(data));
+                this.empInEditMode(this.grid().emps()[0]);
             });
+        }
+
+        toggleEditMode() {
+            this.isInEditMode(!this.isInEditMode())
+        }
+
+        startDeleting(emp) {
+            if (confirm(`Are you certain you want to delete ${emp.name()}?\n\nThis cannot be undone!`)) {
+                alert("TODO");
+            }
+        }
+
+        startEditing(emp) {
+            this.empInEditMode(emp);
+        }
+
+        cancelEditing() {
+            this.empInEditMode().reset();
+            this.empInEditMode(null);
+        }
+
+        commitEditing() {
+            alert("TODO");
         }
     }
 
